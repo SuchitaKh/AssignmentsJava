cf93f4c37dd91233832fa91398c0ccdf32f56c61
ravindra.ankam <ankravi@gmail.com>
2021-05-24 14:17:20 -0700

    Add Table program

---
+++ src/test/java/Assignment/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num= sc.nextInt();
+
+        for(int i=1;i<=10;i++){
+            int prod= num * i;
+            System.out.println(num +" * "+i+" = "+ prod);
+        }
+    }
+}
